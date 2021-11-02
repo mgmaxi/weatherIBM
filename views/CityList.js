3be2d941a1e57e2ed244cfd796677d1890236b9d
mgmaxi <maximiliano.ghirardi@gmail.com>
2021-11-02 13:13:47 -0300

    Get the cities from DB and show them

---
+++ views/CityList.js
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+import {FAB, List, Button, Headline, Paragraph} from 'react-native-paper';
+import axios from 'axios';
+import global from '../styles/global';
+
+const CityList = ({navigation}) => {
+  const [cityList, setCityList] = useState({});
+  const [consultAPI, setConsultAPI] = useState(true);
+
+  // Consult DB for get the cities added to the list
+  useEffect(() => {
+    const getCityList = async () => {
+      try {
+        const url = 'http://10.0.2.2:3000/cityList';
+        const result = await axios.get(url);
+        setCityList(result.data);
+        setConsultAPI(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (consultAPI) {
+      getCityList();
+    }
+  }, [consultAPI]);
+
+  //Fn for navigate to the form
+  const goAddCity = () => {
+    navigation.navigate('AddCity', {setConsultAPI});
+  };
+
+  return (
+    <View style={global.container}>
+      <Headline style={global.title}>City List</Headline>
+      <Paragraph style={global.text}>
+        Add a City or select one from your list to get more details!
+      </Paragraph>
+      <Button
+        style={styles.btnGoAddCity}
+        icon="city"
+        mode="contained"
+        onPress={() => goAddCity()}>
+        Add a City
+      </Button>
+
+      <FlatList
+        data={cityList}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <List.Item
+            title={item.city}
+            description={item.country}
+            onPress={() =>
+              navigation.navigate('CityWeather', {item, setConsultAPI})
+            }
+            left={props => <List.Icon {...props} icon="city" />}
+          />
+        )}
+      />
+      <FAB style={global.fab} small icon="plus" onPress={() => goAddCity()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  btnGoAddCity: {
+    marginTop: 10,
+  },
+});
+
+export default CityList;
